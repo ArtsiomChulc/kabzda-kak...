83ec5f93f540de4071955f85c5f59afaac2f7b38
Artsiom-Chulc <artemorsha@mail.ru>
2023-02-18 00:22:18 +0300

    add props for components AppTitle and Rating

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./accordion/Accordion";
+import Rating from "./rating/Rating";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"wrapper"}>
+            <TitleApp value={"This is APP component"}/>
+            <TitleApp value={"My friends"}/>
+            <Rating value={3}/>
+            <Accordion/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
+
+function TitleApp(props: any) {
+    return (
+        <>
+            <h2>{props.value}</h2>
+        </>
+    )
+}
+
+
+
+
 
 export default App;
